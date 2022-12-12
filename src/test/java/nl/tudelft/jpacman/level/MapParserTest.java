490d1392ec1533711c4820fc1d1c919f6dbfc614
unknown <mkarthikeyanj@gmail.com>
2022-12-12 17:21:49 +0100

    Excercise 20 corrections

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -1,17 +1,19 @@
 package nl.tudelft.jpacman.level;
 
 import nl.tudelft.jpacman.board.BoardFactory;
-import nl.tudelft.jpacman.game.GameFactory;
 import nl.tudelft.jpacman.npc.ghost.Clyde;
 import nl.tudelft.jpacman.npc.ghost.GhostColor;
 import nl.tudelft.jpacman.sprite.PacManSprites;
-import nl.tudelft.jpacman.sprite.Sprite;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 
 public class MapParserTest {
+
+    /**
+     * Creating mocks for MapParser class.
+     */
 
     private LevelFactory levelFactory;
     private BoardFactory boardFactory;
@@ -20,28 +22,35 @@ public class MapParserTest {
 
     MapParser mapParser;
 
+    /**
+     * used mockito framework and defined behaviour for the mock objects.
+     */
     @BeforeEach
-    void setUp(){
-        pacManSprites=new PacManSprites();
-        levelFactory= Mockito.mock(LevelFactory.class);
-        boardFactory=Mockito.mock(BoardFactory.class);
-        mapParser=new MapParser(levelFactory,boardFactory);
+    void setUp() {
+        pacManSprites = new PacManSprites();
+        levelFactory = Mockito.mock(LevelFactory.class);
+        boardFactory = Mockito.mock(BoardFactory.class);
+        mapParser = new MapParser(levelFactory, boardFactory);
         Mockito.when(levelFactory.createGhost()).thenReturn(new Clyde(pacManSprites.getGhostSprite(GhostColor.ORANGE)));
-        Mockito.when(levelFactory.createPellet()).thenReturn(new Pellet(10,pacManSprites.getPelletSprite()));
+        Mockito.when(levelFactory.createPellet()).thenReturn(new Pellet(10, pacManSprites.getPelletSprite()));
         Mockito.when(boardFactory.createGround()).thenReturn(new BoardFactory(pacManSprites).createGround());
         Mockito.when(boardFactory.createWall()).thenReturn(new BoardFactory(pacManSprites).createWall());
 
     }
 
+    /**
+     * nice weather test where board containing expected characters.
+     *
+     */
     @Test
-    void niceWeather(){
-        char [][] grid={{' ',' ','G','.',' ','P','#',' ','.','.',' ','G'},
-            {'P',' ','.','.','#','#','.','#',' ',' ','#',' '},
-            {'.','#','.',' ',' ','#','.',' ','.','.',' ','G'}};
+    void niceWeather() {
+        char[][] grid = {{' ', ' ', 'G', '.', ' ', 'P', '#', ' ', '.', '.', ' ', 'G'},
+            {'P', ' ', '.', '.', '#', '#', '.', '#', ' ', ' ', '#', ' '},
+            {'.', '#', '.', ' ', ' ', '#', '.', ' ', '.', '.', ' ', 'G'}};
         mapParser.parseMap(grid);
-        Mockito.verify(levelFactory,Mockito.times(3)).createGhost();
-        Mockito.verify(levelFactory,Mockito.times(11)).createPellet();
-        Mockito.verify(boardFactory,Mockito.times(29)).createGround();
-        Mockito.verify(boardFactory,Mockito.times(7)).createWall();
+        Mockito.verify(levelFactory, Mockito.times(3)).createGhost();
+        Mockito.verify(levelFactory, Mockito.times(11)).createPellet();
+        Mockito.verify(boardFactory, Mockito.times(29)).createGround();
+        Mockito.verify(boardFactory, Mockito.times(7)).createWall();
     }
 }
