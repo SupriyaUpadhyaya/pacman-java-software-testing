43d316b0b4446543d9b59d6299727b6f2db76d2c
unknown <mkarthikeyanj@gmail.com>
2022-12-12 21:24:15 +0100

    Exercise 22

---
+++ src/test/java/nl/tudelft/jpacman/game/GameTest.java
@@ -0,0 +1,79 @@
+package nl.tudelft.jpacman.game;
+
+import nl.tudelft.jpacman.level.Level;
+import nl.tudelft.jpacman.level.Player;
+import nl.tudelft.jpacman.level.PlayerFactory;
+import nl.tudelft.jpacman.points.PointCalculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GameTest {
+
+    private Level level;
+    private GameFactory gameFactory;
+    private Game game;
+    private PlayerFactory playerFactory;
+    private PointCalculator pointCalculator;
+
+    @BeforeEach
+    void setUp() {
+        level = Mockito.mock(Level.class);
+        playerFactory = Mockito.mock(PlayerFactory.class);
+        pointCalculator = Mockito.mock(PointCalculator.class);
+        gameFactory = new GameFactory(playerFactory);
+        Mockito.when(playerFactory.createPacMan()).thenReturn(Mockito.mock(Player.class));
+
+
+        /**
+         * checking 100% branch coverage.
+          */
+    }
+    @Test
+    /**
+     * isInProgress = false.
+     * isAnyPlayerAlive = true.
+     * remainingPellets > 0.
+     */
+    void niceWeatherStart(){
+        game = gameFactory.createSinglePlayerGame(level, pointCalculator);
+        Mockito.when(level.remainingPellets()).thenReturn(1);
+        Mockito.when(level.isAnyPlayerAlive()).thenReturn(true);
+        assertFalse(game.isInProgress());
+        game.start();
+        assertTrue(game.isInProgress());
+    }
+
+    @Test
+    /**
+     * isInProgress = false.
+     * isAnyPlayerAlive = false.
+     */
+    void gameNotInProgressPlayerDead(){
+        game = gameFactory.createSinglePlayerGame(level, pointCalculator);
+        Mockito.when(level.remainingPellets()).thenReturn(0);
+        Mockito.when(level.isAnyPlayerAlive()).thenReturn(false);
+        assertFalse(game.isInProgress());
+        game.start();
+        assertFalse(game.isInProgress());
+    }
+
+    @Test
+    /**
+     * Only first if block is true.
+     */
+    void gameInProgressPlayerAlive(){
+        game = gameFactory.createSinglePlayerGame(level, pointCalculator);
+        Mockito.when(level.remainingPellets()).thenReturn(1);
+        Mockito.when(level.isAnyPlayerAlive()).thenReturn(true);
+        assertFalse(game.isInProgress());
+        game.start();
+        assertTrue(game.isInProgress());
+        game.start();
+        assertTrue(game.isInProgress());
+    }
+
+}
