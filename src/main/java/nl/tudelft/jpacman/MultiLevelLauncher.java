55278f3fa6b68c903194f9b823db5496183d8814
Supriya Upadhyaya <supriya.upadhyaya@st.ovgu.de>
2023-01-23 11:49:46 +0100

    Fixes : static check style errors

--- src/main/java/nl/tudelft/jpacman/MultiLevelLauncher.java
+++ src/main/java/nl/tudelft/jpacman/MultiLevelLauncher.java
@@ -3,23 +3,25 @@ package nl.tudelft.jpacman;
 import nl.tudelft.jpacman.game.Game;
 import nl.tudelft.jpacman.game.GameFactory;
 import nl.tudelft.jpacman.level.Level;
-/** Used to launch game in 3-level mode
- * Currently functionality is same as single level Launcher
+/**
+ * Used to launch game in 3-level mode.
+ * Currently functionality is same as single level Launcher.
  */
-public class MultiLevelLauncher extends Launcher{
+public class MultiLevelLauncher extends Launcher {
     /**
-     * To create new game
+     * To create new game.
      */
     private Game game;
     /**
      *
-     * @return game
+     * @return game.
      */
     public Game getGame() {
+
         return game;
     }
     /**
-     * Launch multi level game
+     * Launch multi level game.
      */
     public Game makeGame() {
         GameFactory gf = getGameFactory();
